2a78840b461cd0815e5fcdfebd1192159340436c
Joe Formica <joe.v.formica@gmail.com>
2014-01-30 15:00:32 -0500

    Created Home and How it Works pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Twiscount::Application.routes.draw do
+  root "pages#home"
+  get "hiw" =>"pages#hiw"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
