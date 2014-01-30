5662e62147b85caabd0ab1d223c794715a4668d9
Joe Formica <joe.v.formica@gmail.com>
2014-01-30 13:12:17 -0500

    Iniial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twiscount::Application.config.secret_key_base = '2f01c7337cbb4c9964ca22021f3e9805e186ff378ee9301907b08eb2e9ae6e22641c04a12f6670cb578d2819fb60312a93b1d59cc214234558e590b8b9e00723'
